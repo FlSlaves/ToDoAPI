fdae17270ab5fbf071153a80533e022f74382702
maximu211 <63352037+maximu211@users.noreply.github.com>
2023-05-29 19:03:44 +0300

    Try/catch in controller

--- ToDoAPI/Services/ToDoService.cs
+++ ToDoAPI/Services/ToDoService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System.Net;
 using System.Runtime.CompilerServices;
 using ToDoAPI.DTO;
@@ -18,23 +19,29 @@ namespace ToDoAPI.Services
         }
         public async Task DeleteTask(int id)
         {
-
-            var task = await _context.ToDoList1.FindAsync(id);
-            if (task != null)
+            try
             {
+                var task = await _context.ToDoList1.FindAsync(id);
+                if (task != null)
+                {
+                    _context.ToDoList1.Remove(task);
+                    await _context.SaveChangesAsync();
+                }
+
                 _context.ToDoList1.Remove(task);
                 await _context.SaveChangesAsync();
             }
-
-            _context.ToDoList1.Remove(task);
-            await _context.SaveChangesAsync();
-
-
+            catch(DbUpdateException ex) 
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
         public async Task<List<ToDoListModelDTO>> GetTasks(string owner)
         {
-            var tasks = _context.ToDoList1
+            try
+            {
+                var tasks = _context.ToDoList1
                 .Where(t => t.Owner == owner)
                 .Select(t => new ToDoListModelDTO()
                 {
@@ -44,11 +51,19 @@ namespace ToDoAPI.Services
                     Status = t.Status,
                     Owner = t.Owner
                 }).ToListAsync();
-            return await tasks;
+                return await tasks;
+            }
+            catch
+            (DbUpdateException ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
         public async Task InsertTask(string task, string description, string status)
         {
+            try
+            {
                 var toDoList = new ToDoListModel()
                 {
                     Task = task,
@@ -59,18 +74,31 @@ namespace ToDoAPI.Services
 
                 _context.ToDoList1.Add(toDoList);
                 await _context.SaveChangesAsync();
+            }
+            catch
+            (DbUpdateException ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
         public async Task UpdateTaskAs(int id, JsonPatchDocument patchDocument)
         {
-            // Знайдіть запис у базі даних за id
-            var task = await _context.ToDoList1.FindAsync(id);
-            if (task != null)
+            try
             {
-                patchDocument.ApplyTo(task);
-                await _context.SaveChangesAsync();
+                // Знайдіть запис у базі даних за id
+                var task = await _context.ToDoList1.FindAsync(id);
+                if (task != null)
+                {
+                    patchDocument.ApplyTo(task);
+                    await _context.SaveChangesAsync();
+                }
             }
-
+            catch
+            (DbUpdateException ex)
+            {
+                throw new DbUpdateException(ex.Message);
+            }
         }
     }
 }
